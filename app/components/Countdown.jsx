07e98be6597772888d9a95a33915599f619fefe6
Ion Darii <i_darii@yahoo.com>
2016-12-27 11:58:30 -0800

    Addint countdown and styling

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
   render: function(){
     return(
       <div>
-        <h1>Countdown component</h1>
+        <Clock totalSeconds = {13} />
       </div>
     )
   }
